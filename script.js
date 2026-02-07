7ec97d5bfd6af985a2c0cb415902eeb8bb9fdb83
Shlok Vishwakarma <shlokv326@gmail.com>
2026-02-08 01:52:24 +0530

    Added index.html, script.js, style.css

---
+++ script.js
@@ -0,0 +1,24 @@
+// Resume button - open PDF
+document.getElementById("resumeBtn").addEventListener("click", function () {
+  window.open("https://docs.google.com/document/d/12K7L_CF79oH-8968ibJyFUtuXLLX0psyn6P_-sdfrZE/edit?usp=sharing", "_blank");
+});
+
+// LinkedIn button - open profile
+document.getElementById("linkedinBtn").addEventListener("click", function () {
+  window.open("https://www.linkedin.com/in/shlokvishwakarma", "_blank");
+});
+// Instagram button - open profile
+document.getElementById("instagramBtn").addEventListener("click", function () {
+  window.open("https://www.instagram.com/shlokvishwakarmaa", "_blank");
+
+});
+
+// // My Photo - open profile
+// document.getElementById("profilePic").addEventListener("click", function () {
+//   window.open("https://drive.google.com/file/d/1xJuvXZQu8oOr1GXCHibZC6BdGOj5nkO8/view?usp=sharing", "_blank");
+
+// });
+// // GitHub button - open profile
+// document.getElementById("githubBtn").addEventListener("click", function () {
+//   window.open("https://github.com/shlokvishwakarmaa", "_blank");
+// })
